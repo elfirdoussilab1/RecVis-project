886b76df452b83e3110d4efa49cf517efaa1fada
Aymane <afirdoussi123@gmail.com>
2025-01-03 23:58:24 +0100

    plotting heatmaps

--- plot_imp_1.py
+++ plot_imp_1.py
@@ -1,12 +1,21 @@
-# In this file, we will plot the heatmap
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Customize font size and label size
+fontsize = 35  # Change this to adjust title font size
+labelsize = 30  # Change this to adjust axis labels and colorbar labels
+
 # Latex template
-plt.rcParams.update({"text.usetex": True,"font.family": "STIXGeneral"})
+plt.rcParams.update({"text.usetex": True, "font.family": "STIXGeneral"})
+
+# Function to find max coordinate
+def find_max_coord(x, y, z):
+    max_idx = np.argmax(z)
+    return x[max_idx], y[max_idx]
+
 
 # Load the data
-data = np.load("heatmap_data_feasible_3quantities.npz")
+data = np.load("heatmap.npz")
 x_valid = data["x"]
 y_valid = data["y"]
 z1_valid = data["r1"]
@@ -14,27 +23,43 @@ z2_valid = data["r2"]
 z3_valid = data["r3"]
 
 # Create subplots
-fig, axes = plt.subplots(1, 3, figsize=(30, 5), constrained_layout=True)
+fig, axes = plt.subplots(1, 3, figsize=(30, 6), constrained_layout=True)
 
 # Plot each quantity
 heatmap1 = axes[0].tricontourf(x_valid, y_valid, z1_valid, levels=100, cmap='viridis')
-axes[0].set_title('f1(x, y)')
-axes[0].set_xlabel('x')
-axes[0].set_ylabel('y')
-fig.colorbar(heatmap1, ax=axes[0], label='f1(x, y)')
+axes[0].set_title('R@1', fontsize=fontsize)
+axes[0].set_xlabel('$w_0$', fontsize=labelsize)
+axes[0].set_ylabel('$w_1$', fontsize=labelsize)
+axes[0].tick_params(axis = 'x', which = 'both', labelsize = labelsize)
+axes[0].tick_params(axis = 'y', which = 'both', labelsize = labelsize)
+cbar1 = fig.colorbar(heatmap1, ax=axes[0])
+cbar1.ax.tick_params(labelsize=labelsize)
+max_x1, max_y1 = find_max_coord(x_valid, y_valid, z1_valid)
+axes[0].scatter(max_x1, max_y1, color='black', marker='*', s=300)
 
 heatmap2 = axes[1].tricontourf(x_valid, y_valid, z2_valid, levels=100, cmap='plasma')
-axes[1].set_title('f2(x, y)')
-axes[1].set_xlabel('x')
-axes[1].set_ylabel('y')
-fig.colorbar(heatmap2, ax=axes[1], label='f2(x, y)')
+axes[1].set_title('R@5', fontsize=fontsize)
+axes[1].set_xlabel('$w_0$', fontsize=labelsize)
+axes[1].set_ylabel('$w_1$', fontsize=labelsize)
+axes[1].tick_params(axis = 'x', which = 'both', labelsize = labelsize)
+axes[1].tick_params(axis = 'y', which = 'both', labelsize = labelsize)
+cbar2 = fig.colorbar(heatmap2, ax=axes[1])
+cbar2.ax.tick_params(labelsize=labelsize)
+max_x2, max_y2 = find_max_coord(x_valid, y_valid, z2_valid)
+axes[1].scatter(max_x2, max_y2, color='black', marker='*', s=300)
 
 heatmap3 = axes[2].tricontourf(x_valid, y_valid, z3_valid, levels=100, cmap='coolwarm')
-axes[2].set_title('f3(x, y)')
-axes[2].set_xlabel('x')
-axes[2].set_ylabel('y')
-fig.colorbar(heatmap3, ax=axes[2], label='f3(x, y)')
+axes[2].set_title('R_mean', fontsize=fontsize)
+axes[2].set_xlabel('$w_0$', fontsize=labelsize)
+axes[2].set_ylabel('$w_1$', fontsize=labelsize)
+axes[2].tick_params(axis = 'x', which = 'both', labelsize = labelsize)
+axes[2].tick_params(axis = 'y', which = 'both', labelsize = labelsize)
+cbar3 = fig.colorbar(heatmap3, ax=axes[2])
+cbar3.ax.tick_params(labelsize=labelsize)
+max_x3, max_y3 = find_max_coord(x_valid, y_valid, z3_valid)
+axes[2].scatter(max_x3, max_y3, color='black', marker='*', s=300)
 
-# Save figure ...
-# TODO
-print("Figure saved !")
+# Save figure
+path = 'heatmap.pdf'
+fig.savefig(path, bbox_inches='tight')
+print("Figure saved!")
